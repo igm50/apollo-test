dcf265b98dec7a278216be4bce392dddf7570de8
jintz <keisuke.igimi@gmail.com>
2019-12-25 14:13:11 +0900

    feat(client): Apollo Clientを構築する

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useMemo } from 'react'
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'
 
 const App: React.FC = () => {
+  const client = useMemo(
+    () => new ApolloClient({ uri: 'http://localhost:4000' }),
+    []
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApolloProvider client={client}>
+        <div>
+          <h2>
+            My first Apollo app{' '}
+            <span role="img" aria-label="Rocket">
+              🚀
+            </span>
+          </h2>
+          <Books />
+        </div>
+      </ApolloProvider>
     </div>
+  )
+}
+
+const Books: React.FC = () => {
+  const booksQuery = useMemo(
+    () => gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `,
+    []
+  )
+
+  const { loading, error, data } = useQuery(booksQuery)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.books.map(
+    (data: { title: string; author: string }, index: number) => (
+      <div key={data.title}>
+        <h3>book{index + 1}</h3>
+        <p>title:{data.title}</p>
+        <p>author:{data.author}</p>
+      </div>
+    )
   )
 }
 
